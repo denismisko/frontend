6887279461fcab76187a6e82f559d62bc2ece25e
Robik <robert.lehotay@kosickaakademia.sk>
2023-05-23 08:25:55 +0200

    init commit, edited method where user is logged or not

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,23 +1,21 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from '../services/auth.service';
-import { faL } from '@fortawesome/free-solid-svg-icons';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
   constructor(private auth: AuthService, private router: Router) {}
-  canActivate(
+  async canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): boolean{
-      if (!this.auth.isLoggedIn()) {
-        console.log("user is not logged in")
-        this.router.navigate(['/login']);
-        return false;
-      } 
-      console.log("user is logged in")
-      return this.auth.isLoggedIn(); 
+    state: RouterStateSnapshot): Promise<boolean> {
+    if (!this.auth.isLoggedIn()) {
+      console.log("user is not logged in");
+      await this.router.navigate(['/login']);
+      return false;
+    }
+    console.log("user is logged in");
+    return this.auth.isLoggedIn();
   }
 }
